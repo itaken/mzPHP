249d773eb5c241a17ec9bd5d157d940227318be1
regel <regel@regel-PC>
2014-04-26 05:20:57 +0800

    修正部分BUG

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
  * @since 2014-3-21
  * @version 1.0 Beta
  */
-error_reporting(E_ALL);  // 显示所有错误信息
-//error_reporting(E_ERROR | E_WARNING);  // 显示所有错误信息
+//error_reporting(E_ALL);  // 显示所有错误信息
+error_reporting(E_ERROR | E_WARNING);  // 显示所有错误信息
 
 /**
  * 系统调试设置 ( 项目正式部署后请设置为 false )
  */
-define('APP_DEBUG', FALSE);
+define('APP_DEBUG', TRUE);
 
 /**
  * 是否开启路由模式  - TRUE 开启
  */
-define('OPEN_SLINK', TRUE);
+define('OPEN_SLINK', FALSE);
 
 /**
  * 定义项目常量
